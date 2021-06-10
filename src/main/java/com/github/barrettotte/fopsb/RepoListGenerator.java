69e2a5fd162165e07445b56fdf0dfa8fdee356bf
barrettotte <barrettotte@gmail.com>
2021-06-10 10:50:49 -0400

    initial commit

---
+++ src/main/java/com/github/barrettotte/fopsb/RepoListGenerator.java
@@ -0,0 +1,25 @@
+package com.github.barrettotte.fopsb;
+
+import java.io.File;
+
+public class RepoListGenerator {
+
+    public static void main(String[] args) {
+        // TODO: make test data XML
+        // TODO: generate basic PDF w/o QR Code
+        // TODO: generate basic QR code
+        // TODO: hit GitHub API for live data XML
+
+        final File xsltFile = new File("src/main/resources/xslt/repos.xslt");
+        final File xmlFile = new File("src/main/resources/data/temp_data.xml");
+        final String outFo = "src/main/resources/output/repos.fo";
+        final String outPdf = "src/main/resources/output/repos.pdf";
+            
+        try {
+            FopUtils.generateFO(xsltFile, xmlFile, outFo);
+            FopUtils.generatePDF(xsltFile, xmlFile, outPdf);
+        } catch(final Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
